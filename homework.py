72ae642226efaffc10086389c9f6fbe36b901d08
Aleksey <aeksei94@gmail.com>
2023-11-09 14:10:26 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,3 +1,25 @@
+def get_spent_calories(training_type: str, mean_speed: float, weight: float, duration: float):
+    if training_type == "RUN":
+        return (18 * mean_speed + 1.79) * weight / 1000 * duration * 60
+    ...  # Все остальные формулы не описаны, вам их нужно будет реализовать в дочерних классах
+
+
+def get_message(training_type: str, duration: float, distance, speed, calories) -> str:
+    """Формирование строки информационного сообщения.
+
+    training_type - имя класса тренировки
+    duration - длительность тренировки в часах
+    distance - дистанция в километрах, которую преодолел пользователь за время тренировки
+    speed - средняя скорость в км/ч, с которой двигался пользователь
+    calories - количество килокалорий, которое израсходовал пользователь за время тренировки
+    """
+    return (f'Тип тренировки: {training_type}; '
+            f'Длительность: {duration:.3f} ч.; '
+            f'Дистанция: {distance:.3f} км; '
+            f'Ср. скорость: {speed:.3f} км/ч; '
+            f'Потрачено ккал: {calories:.3f}.')
+
+
 class InfoMessage:
     """Информационное сообщение о тренировке."""
     pass
@@ -6,11 +28,13 @@ class InfoMessage:
 class Training:
     """Базовый класс тренировки."""
 
-    def __init__(self,
-                 action: int,
-                 duration: float,
-                 weight: float,
-                 ) -> None:
+    def __init__(self, action: int, duration: float, weight: float) -> None:
+        """Конструктор базового класса тренировки.
+
+        action - количество совершённых действий
+        duration - длительность тренировки в часах
+        weight - вес спортсмена в килограммах
+        """
         pass
 
     def get_distance(self) -> float:
@@ -18,7 +42,7 @@ class Training:
         pass
 
     def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
+        """Получить среднюю скорость движения в км/ч"""
         pass
 
     def get_spent_calories(self) -> float:
@@ -65,4 +89,3 @@ if __name__ == '__main__':
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
-
